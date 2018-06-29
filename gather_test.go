8e1199fac1467df2caa0bfb1b0971af86e35f98a
Aleksandr Razumov <ar@cydev.ru>
2018-06-30 00:17:34 +0300

    gather: add TestAddr_String

--- gather_test.go
+++ gather_test.go
@@ -1,10 +1,28 @@
 package ice
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGather(t *testing.T) {
 	_, err := Gather()
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestAddr_String(t *testing.T) {
+	for _, tt := range []struct {
+		in  Addr
+		out string
+	}{
+		{in: Addr{}, out: "<nil> [0]"},
+		{in: Addr{Zone: "z"}, out: "<nil> (zone z) [0]"},
+	} {
+		t.Run(tt.out, func(t *testing.T) {
+			if tt.in.String() != tt.out {
+				t.Errorf("%q", tt.in)
+			}
+		})
 	}
 }
