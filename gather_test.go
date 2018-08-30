585f9a4a939bdebb806dbee64148b96b44ca92fd
Aleksandr Razumov <ar@gortc.io>
2018-08-30 03:55:02 +0300

    gather: extract to package

--- gather_test.go
+++ gather_test.go
@@ -1,46 +1,10 @@
 package ice
 
-import (
-	"net"
-	"testing"
-)
+import "testing"
 
 func TestGather(t *testing.T) {
 	_, err := Gather()
 	if err != nil {
 		t.Fatal(err)
-	}
-}
-
-func TestAddr_String(t *testing.T) {
-	for _, tt := range []struct {
-		in  Addr
-		out string
-	}{
-		{in: Addr{}, out: "<nil> [0]"},
-		{in: Addr{Zone: "z"}, out: "<nil> (zone z) [0]"},
-	} {
-		t.Run(tt.out, func(t *testing.T) {
-			if tt.in.String() != tt.out {
-				t.Errorf("%q", tt.in)
-			}
-		})
-	}
-}
-
-func TestAddr_ZeroPortAddr(t *testing.T) {
-	for _, tt := range []struct {
-		in  Addr
-		out string
-	}{
-		{in: Addr{}, out: "<nil>:0"},
-		{in: Addr{Zone: "z"}, out: "<nil>%z:0"},
-		{in: Addr{Zone: "z", IP: net.IPv4(127, 0, 0, 1)}, out: "127.0.0.1%z:0"},
-	} {
-		t.Run(tt.out, func(t *testing.T) {
-			if tt.in.ZeroPortAddr() != tt.out {
-				t.Errorf("%q", tt.in.ZeroPortAddr())
-			}
-		})
 	}
 }
