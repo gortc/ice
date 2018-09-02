c143abb93a62b4f3ecd9a2a3bdafbe334a5fd984
Aleksandr Razumov <ar@gortc.io>
2018-09-02 08:25:16 +0300

    internal: improve TestMustParseNet

--- internal/net_test.go
+++ internal/net_test.go
@@ -3,10 +3,18 @@ package internal
 import "testing"
 
 func TestMustParseNet(t *testing.T) {
-	defer func() {
-		if recover() == nil {
-			t.Error("should panic")
+	t.Run("Negative", func(t *testing.T) {
+		defer func() {
+			if recover() == nil {
+				t.Error("should panic")
+			}
+		}()
+		MustParseNet("___")
+	})
+	t.Run("Positive", func(t *testing.T) {
+		net := MustParseNet("0.0.0.0/0")
+		if net.String() != "0.0.0.0/0" {
+			t.Errorf("bad net %s", net)
 		}
-	}()
-	MustParseNet("___")
+	})
 }
