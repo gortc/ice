30c0d70cbf40edada0c05f2204e8c17f4f9850a0
Aleksandr Razumov <ar@gortc.io>
2019-03-12 00:40:23 +0300

    agent: move systemCandidateGatherer to gather.go

--- gather.go
+++ gather.go
@@ -1,10 +1,65 @@
 package ice
 
 import (
+	"net"
+
+	ct "github.com/gortc/ice/candidate"
 	"github.com/gortc/ice/gather"
 )
 
 // Gather via DefaultGatherer.
 func Gather() ([]gather.Addr, error) {
 	return gather.DefaultGatherer.Gather()
+}
+
+type systemCandidateGatherer struct {
+	addr gather.Gatherer
+}
+
+func (g systemCandidateGatherer) gatherUDP(opt gathererOptions) ([]localUDPCandidate, error) {
+	addrs, err := g.addr.Gather()
+	if err != nil {
+		// Failed to gather host addresses.
+		return nil, err
+	}
+	hostAddr, err := HostAddresses(addrs)
+	if err != nil {
+		return nil, err
+	}
+	var candidates []localUDPCandidate
+	for component := 1; component <= opt.Components; component++ {
+		for _, addr := range hostAddr {
+			zeroPort := net.UDPAddr{
+				IP:   addr.IP,
+				Port: 0,
+			}
+			l, err := net.ListenPacket("udp", zeroPort.String())
+			if err != nil {
+				return nil, err
+			}
+			a := l.LocalAddr().(*net.UDPAddr)
+			c := Candidate{
+				Base: Addr{
+					IP:    addr.IP,
+					Port:  a.Port,
+					Proto: ct.UDP,
+				},
+				Type: ct.Host,
+				Addr: Addr{
+					IP:    addr.IP,
+					Port:  a.Port,
+					Proto: ct.UDP,
+				},
+				ComponentID: component,
+			}
+			c.Foundation = Foundation(&c, Addr{})
+			c.Priority = Priority(TypePreference(c.Type), addr.LocalPreference, c.ComponentID)
+			candidates = append(candidates, localUDPCandidate{
+				candidate:  c,
+				conn:       l,
+				preference: addr.LocalPreference,
+			})
+		}
+	}
+	return candidates, nil
 }
